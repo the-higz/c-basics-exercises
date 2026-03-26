95a8e7fa5076b70c4f9809894a3dd4a9f94c1be7
the-higz <guilhermehenrique.sousa@discente.ufcat.edu.br>
2026-03-26 11:20:05 -0300

    Add even_or_odd.c to check integer parity
    Implement a program to check if an integer is even or odd.

---
+++ 02-conditionals/even_or_odd.c
@@ -0,0 +1,29 @@
+/*
+    Programa: even_or_odd.c
+    Descricao:
+    - Recebe um numero inteiro
+    - Verifica se o numero e par ou impar
+    - Utiliza operador modulo e estrutura condicional
+
+    Autor: Guilherme Henrique
+    Data: 2026
+*/
+
+#include <stdio.h>
+
+int main(){
+    int numero;
+
+    printf("\nVerificacao de numero par ou impar!\n");
+    printf("\nInsira um numero inteiro: ");
+    scanf("%d", &numero);
+
+    // Verifica se o numero e impar ou par
+    if(numero % 2 == 1){
+        printf("\nO numero %d eh impar\n", numero);
+    } else{
+        printf("\nO numero %d eh par\n", numero);
+    }
+
+    return 0;
+}
