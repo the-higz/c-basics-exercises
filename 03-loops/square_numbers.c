5564f67062092eb2f462f0cfe719d8a0f096234f
the-higz <guilhermehenrique.sousa@discente.ufcat.edu.br>
2026-03-26 10:54:04 -0300

    Add square_numbers.c to calculate squares of numbers
    This program displays the square of numbers from 0 to 10 and allows the user to input their own integers to calculate their squares until 0 is entered.

---
+++ 03-loops/square_numbers.c
@@ -0,0 +1,41 @@
+/*
+    Programa: square_numbers.c
+    Descricao: 
+    - Exibe o quadrado dos numeros de 0 a 10
+    - Permite que o usuario digite numeros e calcula seus quadrados
+    - O programa encerra quando o usuario digita 0
+
+    Autor: Guilherme Henrique
+    Data: 2026
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    // Parte 1: exibe o quadrado dos numeros de 0 a 10
+    for (int i = 0; i <= 10; i++) {
+        printf("O quadrado de %d e: %d\n", i, i * i);
+    }
+
+    int num;
+
+    printf("\nAgora digite seus proprios numeros inteiros (digite 0 para encerrar):\n");
+
+    // Parte 2: leitura de numeros do usuario
+    // O loop continua ate que o usuario digite 0
+    while (1) {
+        printf("\nInsira um numero: ");
+        scanf("%d", &num);
+
+        // Condicao de parada
+        if (num == 0) {
+            break;
+        }
+
+        // Calcula e exibe o quadrado do numero informado
+        printf("O quadrado de %d e: %d\n", num, num * num);
+    }
+
+    return 0;
+}
