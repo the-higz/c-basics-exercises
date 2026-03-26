a31ed22ab14f10930010ca0c895111c3657293ba
the-higz <guilhermehenrique.sousa@discente.ufcat.edu.br>
2026-03-26 11:32:24 -0300

    Add salary calculation program in C
    Calculates total annual payment based on normal and extra hours worked.

---
+++ 01-input-output/salary_calculation.c
@@ -0,0 +1,32 @@
+/*
+    Programa: salary_calculation.c
+    Descricao:
+    - Recebe a quantidade de horas normais e extras trabalhadas
+    - Calcula o pagamento total anual
+    - Considera R$10 por hora normal e R$15 por hora extra
+
+    Autor: Guilherme Henrique
+    Data: 2026
+*/
+
+#include <stdio.h>
+
+int main(){
+    int horasNormais, horasExtras;
+    int pagamentoNormal, pagamentoExtra, total;
+
+    printf("Quantas horas normais e horas extras voce trabalhou este ano: ");
+    scanf("%d %d", &horasNormais, &horasExtras);
+
+    // Calcula os pagamentos
+    pagamentoNormal = horasNormais * 10;
+    pagamentoExtra = horasExtras * 15;
+
+    // Soma total
+    total = pagamentoNormal + pagamentoExtra;
+
+    // Exibe o resultado
+    printf("\nVoce recebeu R$ %d,00 este ano\n", total);
+
+    return 0;
+}
