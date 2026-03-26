0bb000747d257c6d5a6126c46ffda762d28cea36
the-higz <guilhermehenrique.sousa@discente.ufcat.edu.br>
2026-03-26 11:30:58 -0300

    Add max_and_count.c to find max and count occurrences
    This program reads 10 integers, identifies the maximum value, and counts how many times it was entered.

---
+++ 03-loops/max_and_count.c
@@ -0,0 +1,44 @@
+/*
+    Programa: max_and_count.c
+    Descricao:
+    - Recebe 10 numeros inteiros
+    - Identifica o maior valor
+    - Conta quantas vezes o maior numero foi informado
+    - Utiliza estrutura de repeticao e condicional
+
+    Autor: Guilherme Henrique
+    Data: 2026
+*/
+
+#include <stdio.h>
+
+int main(){
+    int num, maior, contador = 0;
+
+    // Loop para leitura dos 10 numeros
+    for(int i = 0; i < 10; i++){
+        printf("Digite o %d numero: ", i + 1);
+        scanf("%d", &num);
+
+        // Primeira leitura define o maior inicial
+        if(i == 0){
+            maior = num;
+            contador = 1;
+        }
+        // Novo maior encontrado
+        else if(num > maior){
+            maior = num;
+            contador = 1;
+        }
+        // Numero igual ao maior atual
+        else if(num == maior){
+            contador++;
+        }
+    }
+
+    // Exibe os resultados
+    printf("\nO maior numero eh: %d\n", maior);
+    printf("Ele foi lido %d vez(es).\n", contador);
+
+    return 0;
+}
